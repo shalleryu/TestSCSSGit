22fa7372f25e2646bf3199cef947892814acc3a2
yuxiaole <shalleryu@163.com>
2019-06-28 14:22:02 +0800

    beautify my folder

--- test.old/1_old_gulp/gulp/obsolete/guide.js
+++ test.old/1_old_gulp/gulp/obsolete/guide.js
@@ -1,5 +1,5 @@
 'use strict';
-const SCSSHelper = require('./scsshelper.js.js');
+const SCSSHelper = require('./scsshelper.js.js.js.js.js.js');
 let helper = new SCSSHelper('guide');
 helper.config({
     scsspath: 'guide/*.scss',
--- test.old/gulp/obsolete/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const SCSSHelper = require('./scsshelper.js.js');
-for (let index = 1; index <= 4; index++) {
-    let helper = new SCSSHelper(`test${index}`);
-    helper.config({
-        scsspath: `test${index}/src/*.scss`,
-        csspath: `test${index}/css`
-    }).runtasks();
-}
