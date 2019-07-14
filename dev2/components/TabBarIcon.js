4fa73005034d08de9ea3f4b832cb74557ab8f556
shalleryu <shalleryu@163.com>
2019-07-14 16:49:16 +0800

    remove expo

--- dev2/components/TabBarIcon.js
+++ dev2/components/TabBarIcon.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Ionicons } from '@expo/vector-icons';
+import { View } from 'react-native';
 
 import Colors from '../constants/Colors';
 
 export default function TabBarIcon(props) {
   return (
-    <Ionicons
+    <View
       name={props.name}
       size={26}
       style={{ marginBottom: -3 }}
