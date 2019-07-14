c4c099991193deedefe0e5e76e453549246357a2
shalleryu <shalleryu@163.com>
2019-07-14 12:43:10 +0800

    bakup test22

--- test.old/test22/README.md
+++
@@ -1,9 +0,0 @@
-expo init 最简单的模板页
-expo eject以后看看能不能web
-
-1.Failed to capture fingerprint of output files for task 
-cd android
-gradlew clean
-cd ..
-react-native run-android
-https://github.com/wix/react-native-navigation/issues/4858
--- test.old/test22/constants/Layout.js
+++
@@ -1,6 +0,0 @@
-import { Dimensions } from 'react-native';
-const window = Dimensions.get('window');
-const width = window.width;
-const height = window.height;
-
-export default { window: { width, height }, isSmallDevice: width < 375, };
